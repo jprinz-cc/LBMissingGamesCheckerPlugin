9e154c37e2c3852553496125413e6de55dd7a180
JP <jp@johnprinz.com>
2024-09-01 16:28:18 -0400

    Add project files.

---
+++ LBMissingGamesCheckerPlugin/MissingGamesChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using Unbroken.LaunchBox.Plugins;
+
+namespace LBMissingGamesCheckerPlugin
+{
+    public class MissingGamesChecker : ISystemMenuItemPlugin
+    {
+        public string Caption => "Missing Games Checker";
+        public string Description => "Check which games you have and don't have for a selected platform.";
+        public string Version => "1.0";
+        public string MenuItemTitle => "Missing Games Checker";
+
+        public System.Drawing.Image IconImage
+        { 
+            get { return null; }
+        }
+
+        public bool ShowInLaunchBox 
+        { 
+            get { return true; }
+        }
+
+        public bool ShowInBigBox
+        { get { return false; } }
+
+        public bool AllowInBigBoxWhenLocked
+        { get { return false; } }
+
+        public void OnSelected()
+        {
+            MessageBox.Show("Hello World!");
+        }
+    }
+}
