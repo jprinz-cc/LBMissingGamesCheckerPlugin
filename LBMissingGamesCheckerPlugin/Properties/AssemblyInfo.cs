17cd08e16d2ad80fcec77182879401d1085d7027
JP <jp@johnprinz.com>
2024-09-05 14:00:36 -0400

    Updated project info;

--- LBMissingGamesCheckerPlugin/Properties/AssemblyInfo.cs
+++ LBMissingGamesCheckerPlugin/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LBMissingGamesCheckerPlugin")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Check which games you have and don't have for a selected platform in LaunchBox.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("JP Consulting")]
 [assembly: AssemblyProduct("LBMissingGamesCheckerPlugin")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
